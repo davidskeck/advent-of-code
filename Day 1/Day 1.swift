6455a6e7fd90563294f0042a3e9fae1a1ed42d38
David Keck <davidskeck@fastmail.com>
2018-12-01 23:24:36 -0500

    Day 1 complete!
    Some housekeeping for 2018

---
+++ Day 1/Day 1.swift
@@ -0,0 +1,69 @@
+// Notes:
+// Wow. This challenge was TOUGH. Mainly because I implemented the whole thing
+// in command-line Swift. I'm using the latest snapshot too (November 30th).
+// The hardest parts were getting the file to open, which is super easy in other languages
+// and finding a fast way to do the second part, which requires a lot of iterating.
+// I wonder if I got into trouble because of Swift's problems with large arrays.
+// Good challange overall, really demonstrates how important it can be to have the
+// right data structure for certain problems. :D
+
+import Foundation
+
+let inputFilePath = URL(fileURLWithPath: FileManager.default.currentDirectoryPath + "/Day 1/input.txt")
+
+do {
+    let text = try String(contentsOf: inputFilePath, encoding: .utf8)
+    let lines: [String] = text.components(separatedBy: "\n")
+    var totalValue = 0
+    var firstIterationTotal = 0
+    var firstRepeatingNumber = 0
+    var firstRepeatingNumberFound = false
+    var valueHistory = Set<Int>()
+    valueHistory.insert(totalValue)
+    for line in lines {
+        if let currentNumber = Int(line) {
+            totalValue += currentNumber
+
+            if !firstRepeatingNumberFound {
+                if valueHistory.contains(totalValue) {
+                    firstRepeatingNumber = totalValue
+                    firstRepeatingNumberFound = true
+                }
+            }
+
+            valueHistory.insert(totalValue)
+        } else {
+            print("Uh oh. Error occured.")
+        }
+    }
+
+    firstIterationTotal = totalValue
+
+    while !firstRepeatingNumberFound {
+        for line in lines {
+            if let currentNumber = Int(line) {
+                totalValue += currentNumber
+
+                if !firstRepeatingNumberFound {
+                    if valueHistory.contains(totalValue) {
+                        firstRepeatingNumber = totalValue
+                        firstRepeatingNumberFound = true
+                        break
+                    }
+                }
+
+                valueHistory.insert(totalValue)
+            } else {
+                print("Uh oh. Error occured.")
+            }
+        }
+    }
+
+    print("Challenge 1:", firstIterationTotal)
+    if firstRepeatingNumberFound {
+        print("Challenge 2:", firstRepeatingNumber)
+    }
+}
+catch {
+    print(error)
+}
--- README.md
+++ README.md
@@ -1,7 +1,8 @@
-# Advent of Code 2017
+# Advent of Code 2018
 
-My solutions to the Advent of Code 2017.
+My solutions to the Advent of Code 2018.
 
 http://adventofcode.com
 
+[AOC 2017 is here.](/AOC_2017)
 [AOC 2016 is here.](/AOC_2016)
