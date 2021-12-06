74a825372e2567c7abc881fbad1b2e946184d1b9
David Keck <davidskeck@fastmail.com>
2021-12-06 17:54:18 -0500

    Completed day06

--- AOC_2021/06.py
+++ AOC_2021/06.py
@@ -2,71 +2,52 @@
 # Day 6
 
 import os
-from multiprocessing.pool import Pool
-
+from collections import Counter
 
 EXAMPLE_DAYS = 18
 PART_ONE_DAYS = 80
 PART_TWO_DAYS = 256
 
-class LanternFish:
-    def __init__(self, age):
-        self.age = age
-        self.should_create_newborn = False
 
-    def step(self):
-        if self.age == 0:
-            self.age = 6
-            self.should_create_newborn = True
-        else: 
-            self.age -= 1
-
-
-def run_lanternfish_sim(population, days=PART_TWO_DAYS):
-    lantern_fish = population
-    
+def simulate_population(population, days):
     for i in range(days):
-        newborns = []
-        for fish in lantern_fish:
-            fish.step()
-            
-            if fish.should_create_newborn:
-                newborns.append(LanternFish(8))
-                fish.should_create_newborn = False
-        lantern_fish.extend(newborns)
+        new_pop = dict.fromkeys(range(9), 0)
+        
+        new_pop[8] = population[0]
+        
+        for key, val in population.items():
+            if key > 0:
+                new_pop[key - 1] = val
+        
+        new_pop[6] += population[0]
 
-    return len(lantern_fish)
+        population = new_pop
+            
+    return population
 
 
 def main():
-    pool = Pool(processes=8)
     with open(f"input{os.sep}day06.txt") as puzzle_data:
-        puzzle_input = puzzle_data.read().split(',')
+        puzzle_input = [int(timer) for timer in puzzle_data.read().split(',')]
     
-    lantern_fish = []
-    for age in puzzle_input:
-        lantern_fish.append(LanternFish(int(age)))
+    lantern_timers = Counter(puzzle_input)
+    lantern_timers.update({6: 0, 7: 0, 8: 0, 0: 0})
 
-    split_populations = []
-    num_processes = 8
-    minimum_slice_size = int(len(lantern_fish) / num_processes)
-    for i in range(num_processes):
-        if i < num_processes - 1:
-            split_populations.append(lantern_fish[i * minimum_slice_size: i * minimum_slice_size + minimum_slice_size])
-        else:
-            split_populations.append(lantern_fish[i * minimum_slice_size:])
-    for pop in split_populations:
-        print(len(pop))
-    results = pool.map(run_lanternfish_sim, split_populations)
-
+    population = simulate_population(lantern_timers, PART_ONE_DAYS)
+    
     total_fish = 0
-    for result in results:
-        total_fish += result
-    #total_fish = run_lanternfish_sim(lantern_fish, PART_TWO_DAYS)    
- 
-    print(f"Part One: {total_fish}")
+    for key, val in population.items():
+        total_fish += val
     
-#    print(f"Part Two: {}")
+    print(f"Part Two: {total_fish}")
+
+    population = simulate_population(population, PART_TWO_DAYS - PART_ONE_DAYS)
+    
+    total_fish = 0
+    for key, val in population.items():
+        total_fish += val
+    
+    print(f"Part Two: {total_fish}")
 
 
 if __name__ == "__main__":
