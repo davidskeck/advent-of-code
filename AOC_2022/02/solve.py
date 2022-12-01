9d340814dd9773b73d1bb1d79a70cb80e5815e56
David Keck <davidskeck@fastmail.com>
2022-12-01 00:47:10 -0500

    minor changes to template to add easy access to example input

---
+++ AOC_2022/02/solve.py
@@ -0,0 +1,16 @@
+# Advent of Code 2022
+# Day 2
+
+input_file = "input.txt"
+#input_file = "example.txt"
+
+
+def main():
+    with open(input_file) as puzzle_data:
+        puzzle_input = puzzle_data.read().split()
+
+    print(puzzle_input)
+
+
+if __name__ == "__main__":
+    main()
