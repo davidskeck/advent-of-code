e38f5fe8054d420fce901135e253c9ea194f333f
David Keck <davidskeck@fastmail.com>
2022-12-03 00:23:45 -0500

    add day 3 solutions

--- AOC_2022/03/solve.py
+++ AOC_2022/03/solve.py
@@ -13,16 +13,46 @@ def main(input_file):
     with open(input_file) as puzzle_data:
         puzzle_input = puzzle_data.read().split('\n')
 
-    answer = 0
 
     # Part One
+    answer = 0
+    for rucksack in puzzle_input:
+        if rucksack == "":
+            continue
+        size = len(rucksack) // 2
+        compartment_one = rucksack[0:size]
+        compartment_two = rucksack[size:]
 
+        same_char = None
+        for character in compartment_one:
+            if character in compartment_two:
+                same_char = character
+                break
+
+        if same_char.islower():
+            answer += ord(same_char) - 96
+        else:
+            answer += ord(same_char) - 38
 
     pyperclip.copy(answer)
     print(f"{input_file.split('.')[0].capitalize()} Part One: {answer}")
 
-    # Part Two
 
+    # Part Two
+    answer = 0
+    for ruck_index in range(0, len(puzzle_input) - 1, 3):
+        ruck_one, ruck_two, ruck_three = tuple(puzzle_input[ruck_index: ruck_index + 3])
+
+        same_char = None
+        for character in ruck_one:
+            if character in ruck_two and character in ruck_three:
+                same_char = character
+                break
+
+        if same_char.islower():
+            answer += ord(same_char) - 96
+        else:
+            answer += ord(same_char) - 38
 
     pyperclip.copy(answer)
     print(f"{input_file.split('.')[0].capitalize()} Part Two: {answer}")
