2a7c1eb231ae451b6aab7d67501155f0ecaffca7
David Keck <davidskeck@fastmail.com>
2022-12-06 00:21:14 -0500

    add day 6

---
+++ AOC_2022/06/solve.py
@@ -0,0 +1,52 @@
+# Advent of Code 2022
+# Day 6
+
+from collections import Counter
+import pyperclip
+
+input_files = [
+    "example.txt",
+    "input.txt"
+]
+
+
+def find_marker(puzzle_input, marker_size):
+    current_chars = []
+    for char_index in range(len(puzzle_input[0]) - marker_size):
+        current_chars = puzzle_input[0][char_index: char_index + marker_size]
+        is_unique = False
+        for letter, count in Counter(current_chars).items():
+            if count > 1:
+                is_unique = False
+                break
+            else:
+                is_unique = True
+
+        if is_unique:
+            answer = char_index + marker_size
+            break
+
+    return answer
+
+
+def main(input_file):
+    with open(input_file) as puzzle_data:
+        puzzle_input = [line for line in puzzle_data.read().split('\n') if line != ""]
+
+    # Part One
+    answer = 0
+    answer = find_marker(puzzle_input, 4)
+    pyperclip.copy(answer)
+    print(f"{input_file.split('.')[0].capitalize()} Part One: {answer}")
+
+    # Part Two
+    answer = 0
+    answer = find_marker(puzzle_input, 14)
+
+    pyperclip.copy(answer)
+    print(f"{input_file.split('.')[0].capitalize()} Part Two: {answer}")
+
+
+if __name__ == "__main__":
+    for file in input_files:
+        main(file)
