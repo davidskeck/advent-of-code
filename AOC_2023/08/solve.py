c23f93f7aea2c840c8e977507c2d7ddc3a7bacc7
David Keck <davidskeck@fastmail.com>
2023-12-08 08:57:32 -0500

    working part 2 solution for day 8, updated code to work with correct examples for different parts

--- AOC_2023/08/solve.py
+++ AOC_2023/08/solve.py
@@ -4,6 +4,8 @@
 import time
 
 import pyperclip
+import numpy as np
+
 
 input_files = [
     "example.txt",
@@ -39,49 +41,62 @@ def main(input_file):
     for index, node in enumerate(nodes):
         node_dict[node.name] = node
     
-    # end_found = False
-    # current_node = None
-    # for node in nodes:
-    #     if node.name == "AAA":
-    #         current_node = node
-    #         break
-
-    # while not end_found:
-    #     for direction in list(instructions):
-    #         answer += 1
-    #         next_node_name = current_node.elements[direction_dict.get(direction)]
-    #         for node in nodes:
-    #             if node.name == next_node_name:
-    #                 current_node = node
-    #                 break
-    #         if current_node.name == "ZZZ":
-    #             end_found = True
-    #             break
+    
+    current_node = node_dict["AAA"]
+    end_found = False
+    while not end_found:
+        for direction in list(instructions):
+            answer += 1
+            next_node_name = current_node.elements[direction_dict.get(direction)]
+            
+            if next_node_name == "ZZZ":
+                end_found = True
+                break
+            else:
+                current_node = node_dict[next_node_name]
 
     pyperclip.copy(answer)
     print(f"{input_file.split('.')[0].capitalize()} Part One: {answer}")
 
     # Part Two
+    if input_file == "example.txt":
+        input_file = "example_p2.txt"
+        with open(input_file) as input_data:
+            puzzle_input = [line for line in input_data.read().split('\n') if line != ""]
+        
+        instructions = puzzle_input[0]
+        nodes = []
+        for line in puzzle_input:
+            if '=' in line:
+                nodes.append(Node(line))
+        
+        node_dict = {}
+        for index, node in enumerate(nodes):
+            node_dict[node.name] = node
+
     answer = 0
 
     current_nodes = []
     for node in nodes:
         if node.name[-1] == "A":
-            current_nodes.append(node)
+            current_nodes.append((node, None))
 
     end_found = False
     while not end_found:
         for direction in list(instructions):
             answer += 1
-            curr_nodes_pos = ""
+            end_found = True
             for index, curr_node in enumerate(current_nodes):
-                next_node_name = curr_node.elements[direction_dict.get(direction)]
-                current_nodes[index] = node_dict[next_node_name]
-                curr_nodes_pos += next_node_name[-1]
-            
-            if curr_nodes_pos == "Z" * len(current_nodes):
-                end_found = True
-                break
+                curr_node, distance = curr_node
+                if distance is None:
+                    end_found = False
+                    next_node_name = curr_node.elements[direction_dict.get(direction)]
+                    if next_node_name[-1] == "Z":
+                        distance = answer
+                    current_nodes[index] = (node_dict[next_node_name], distance)
+    
+    multiples = [curr_node[1] for curr_node in current_nodes]
+    answer = int(np.lcm.reduce(multiples))
 
     pyperclip.copy(answer)
     print(f"{input_file.split('.')[0].capitalize()} Part Two: {answer}")
